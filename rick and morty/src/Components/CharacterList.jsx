328417bf82d9952b41a73e198cc5030416ba4ed9
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2024-06-24 22:42:47 -0300

    search funcional

--- rick and morty/src/Components/CharacterList.jsx
+++ rick and morty/src/Components/CharacterList.jsx
@@ -1,20 +1,38 @@
-const fetchCharacters = (name) => {   
-    setLoading(true);
-       fetch(`https://rickandmortyapi.com/api/character/?name=${name}`)
-         .then(response => response.json())
-         .then(data => {
-           setCharacters(data.results || []);
-           setLoading(false);
-         })
-         .catch(error => {
-           setError(error);
-           setLoading(false);
-         });
-     };
-     
+import React, { useState } from 'react';
 
-export default function CharacterSearch(){
-    return(
-        <h1></h1>
-    )
+export default function CharacterList() {
+  const [name, setName] = useState('');
+  const [characters, setCharacters] = useState([]);
+
+  const fetchCharacters = (name) => {
+    setLoading(true);
+    fetch(`https://rickandmortyapi.com/api/character/?name=${name}`)
+      .then(response => response.json())
+      .then(data => {
+        setCharacters(data.results || []);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchCharacters(name);
+  };
+
+  return (
+    <div>
+        <div className='galeria'>
+          {characters.map(character => (
+          <div key={character.id} className='personagem'>
+              <img className='personagem-img' src={character.image} alt={character.name} />
+              <h3 className='name'>{character.name}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
